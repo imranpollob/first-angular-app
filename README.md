dc930fa91cfdc8b2391bf1213c1593ee08b9bc80
imranpollob <polboy777@gmail.com>
2024-02-20 23:23:19 -0500

    adding an interface

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ Used commands:
 ng new first-angular-app
 ng serve
 ng generate component home --inline-template --skip-tests
-
+ng generate component housingLocation --inline-template --skip-tests
+ng generate interface housinglocation
 ```
 
---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
+
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
